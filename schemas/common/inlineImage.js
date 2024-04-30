2881cee2c0fe259476d4e8d42a9356887a076a9f
samuelgoddard <hello@samgoddard.co.uk>
2024-04-30 10:32:19 +0100

    add link

--- schemas/common/inlineImage.js
+++ schemas/common/inlineImage.js
@@ -20,6 +20,49 @@ export default {
           { title: "Wrap Right", value: "wrapright" },
         ],
       }
+    },
+    {
+      type: "object",
+      name: "customLink",
+      fields: [
+        {
+          title: 'Link?',
+          name: 'linkToggle',
+          description: 'Toggle this on if you want to link this image somewhere',
+          type: 'boolean',
+          validation: Rule => Rule.required()
+        },
+        {
+          title: 'Internal Link?',
+          name: 'internal',
+          description: 'Toggle this on if you want to link internally within the website, rather than link to an external source',
+          type: 'boolean',
+          hidden: ({ parent, value }) => !value && (parent?.linkToggle == false),
+        },
+        {
+          name: 'internalLink',
+          type: 'reference',
+          hidden: ({ parent, value }) => !value && (parent?.internal == false),
+          title: 'Internal Link',
+          to: [
+            {type: 'news'},
+            {type: 'about'},
+            {type: 'contact'},
+            {type: 'newsLanding'},
+            {type: 'home'},
+            {type: 'whatsOn'}
+          ]
+        },
+        {
+          title: 'External Link',
+          hidden: ({ parent, value }) => !value && (parent?.internal == true),
+          name: 'externalLink',
+          type: 'url',
+          validation: Rule => Rule.uri({
+            scheme: ['http', 'https', 'mailto', 'tel']
+          })
+        }
+      ]
     }
   ],
   preview: {

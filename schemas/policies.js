faab67756692d4cddd0156e330d95c1515c19a7c
liamfrogspark <rafael.cardoso@thedigitalmaze.com>
2025-03-27 13:39:36 +0000

    adding new schema

--- schemas/policies.js
+++ schemas/policies.js
@@ -15,7 +15,65 @@ export default {
       title: "Content",
       name: "content",
       type: 'array', 
-      of: [{type: 'block'}],
+      of: [{type: 'block',      marks: {
+        decorators: [
+          {title: 'Italic', value: 'em'},
+          {title: 'Bold', value: 'strong'},
+          {title: 'Display Font', value: 'strike-through' }
+        ],
+        annotations: [
+          {
+            name: "anchorLink",
+            icon: FaAnchor,
+            title: "Anchor Link",
+            type: "object",
+            fields: [
+              {
+                name: "href",
+                title: "URL",
+                type: "string", // Change to string if you want anchor IDs (e.g., "#section")
+                validation: (Rule) => Rule.required(),
+              },
+            ],
+          },
+          {
+            name: "anchorId",
+            icon: FaAnchorLock,
+            title: "Anchor ID",
+            type: "object",
+            fields: [
+              {
+                name: "anchorId", // Match with serializer naming
+                title: "Anchor ID",
+                type: "string",
+                validation: (Rule) => Rule.required(),
+              },
+            ],
+            description: "Add a unique ID for internal linking.",
+          },
+          {
+            name: 'link',
+            type: 'object',
+            title: 'Link',
+            fields: [
+              {
+                name: 'href',
+                title: 'URL',
+                type: 'url'
+              },
+              {
+                name: 'blank',
+                title: "Open in new tab?",
+                type: "boolean",
+                initialValue: false
+              }
+            ],
+            components: {
+              annotation: ExternalLinkRenderer
+            }
+          },
+        ]
+      }}],
     },
     {
       name: 'slug',

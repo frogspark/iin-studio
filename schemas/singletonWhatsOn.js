5e531924ab78d2b20d475b8f580ac01bcc09a2d1
rafael cardoso <rafael.cardoso@thedigitalmaze.com>
2025-10-27 15:52:05 +0000

    Add styles and decorators to block type in schema
    Introduced support for H1, H2, H3, and normal styles, as well as strong, emphasis, and strike-through decorators to the block type in singletonWhatsOn.js. This enhances text formatting options in the schema.

--- schemas/singletonWhatsOn.js
+++ schemas/singletonWhatsOn.js
@@ -84,7 +84,18 @@ export default {
       of: [
         {
           type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+          ],
           marks: {
+            decorators: [
+              {title: 'Strong', value: 'strong'},
+              {title: 'Emphasis', value: 'em'},
+              {title: 'Strike', value: 'strike-through'},
+            ],
             annotations: [
               {
                 name: "anchorLink",
